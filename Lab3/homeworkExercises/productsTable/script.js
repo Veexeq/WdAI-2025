8dedfe343c676065c3dd849c4f733e0a0e86d459
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-11-17 22:45:42 +0100

    Updated JS logic, which now creates and places HTML elements from JSON.

--- Lab3/homeworkExercises/productsTable/script.js
+++ Lab3/homeworkExercises/productsTable/script.js
@@ -1,3 +1,47 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
+document.addEventListener("DOMContentLoaded", async () => {
+    const productsTable = document.getElementById("products-table");
+
+    async function fetchJSON(url) {
+        const response = await fetch(url);
+        const productJSON = await response.json();
+        return productJSON;
+    }
+
+    function createProductElement(photoLink, title, description) {
+        const newRow = document.createElement("tr");
+        newRow.classList.add("product");
+        
+        const imageCell = document.createElement("td");
+        const image = document.createElement("img");
+        image.src = photoLink;
+        image.alt = title;
+        imageCell.appendChild(image);
+
+        const titleCell = document.createElement("td");
+        titleCell.textContent = title;
+
+        const descriptionCell = document.createElement("td");
+        descriptionCell.textContent = description;
+
+        newRow.appendChild(imageCell);
+        newRow.appendChild(titleCell);
+        newRow.appendChild(descriptionCell);
+
+        return newRow;
+    }
+
+    const productsJSON = await fetchJSON("https://dummyjson.com/products");
+    const numOfPositions = 30;
+
+    for (let i = 0; i < numOfPositions; i++) {
+        const productData = productsJSON.products[i];
+        const product = createProductElement(
+            productData.images[0],
+            productData.title,
+            productData.description
+        );
+
+        productsTable.appendChild(product);
+    }
+
 });
