db9ea07ca3ae65a9aec4d79c35d0ea057ae3fd08
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-11-20 00:59:02 +0100

    Added baseline for pipe entity, for now only the bottom pipe.

---
+++ Lab3/flappyBird/js/Pipe.js
@@ -0,0 +1,38 @@
+export class Pipe {
+
+    constructor(game) {
+        this.game = game;
+        
+        this.width = 52;
+        this.height = 320;
+        this.gap = 150;
+        
+        const minHeight = 100;
+        const maxHeight = this.game.height - this.gap - minHeight; 
+
+        this.x = this.game.width;
+        this.y = minHeight + Math.random() * (maxHeight - minHeight);
+
+        this.sprite = new Image();
+        this.sprite.src = "Flappy_Bird/pipe-green.png";
+        
+        this.markedForDeletion = false;
+
+        this.velocity = 2;
+    }
+
+    update() {
+        this.x -= this.velocity;
+        
+        if (this.x < -this.width) {
+            this.markedForDeletion = true;
+        }
+    }
+
+    draw() {
+        const ctx = this.game.ctx;
+        if (!this.image.complete) return;
+
+        ctx.drawImage(this.image, this.x, this.y + this.gap);
+    }
+}
