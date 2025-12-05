e126cbec358d25319efb27981083eb03d2ba183a
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-06 00:44:26 +0100

    Added comments explaining the behavior of endpoint handlers.

--- Lab4/MikroserwisyProjekt/books-service/app.js
+++ Lab4/MikroserwisyProjekt/books-service/app.js
@@ -4,6 +4,20 @@ const PORT = 3001;
 
 // Simple test endpoint
 app.get('/api/books', (req, res) => {
+    console.log("Sent books request.");
+    // 'req' is the variable holdinig info regarding request, while
+    // 'res' is the pre-filled object with response. We modify the response
+    // to our liking, rather than create it from scratch. 
+    //
+    // Example of modifying the default behavior. Normally if the handler (app.get in this case)
+    // executes all of its logic and doesn't throw an exception, status code is 200.
+    // res.status(201);
+    //
+    // We modify the json of the response (there is no default json in contrast to above text).
+    // VERY IMPORTANT: res.json(...) is A TERMINAL METHOD. It 'seals' the response and sends it,
+    // we can't do anything below this.
+    // An alternative method for sending a response is res.send(...) which doesn't force
+    // a JSON format (we can send a HTML tag for example).
     res.json({message: "List of books here (microservice no. 1)."});
 })
 
