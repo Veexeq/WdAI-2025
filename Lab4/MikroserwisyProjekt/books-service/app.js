e12d5d165812cfd3619a7a73fd371632ab50bbc5
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-06 20:14:48 +0100

    Added 'get all books', 'get a specific book by ID' and 'delete a specific book by ID' endpoints. Fixed formatting.

--- Lab4/MikroserwisyProjekt/books-service/app.js
+++ Lab4/MikroserwisyProjekt/books-service/app.js
@@ -14,7 +14,59 @@ const sequelize = require('./config/database');
 // First middleware, parse JSONs in POST reqs
 app.use(express.json());
 
-// ENDPOINT: 'add book'
+// GET-ENDPOINT: 'select all books'
+app.get('/api/books', async (req, res) => {
+
+    try {
+
+        const allBooks = await Book.findAll();
+
+        res.status(200)
+           .json(allBooks);
+
+    } catch (error) {
+
+        // Handle error in a general way for now
+        res.status(500)
+           .json({ error: error.message });
+    }
+});
+
+// GET-ENDPOINT: 'select a specific book'
+app.get('/api/books/:id', async (req, res) => {
+
+    try {
+
+        // The endpoint was defined as ':id', so the
+        // req.params JSON will hold { id: '5' }
+        const id = req.params.id;
+        const book = await Book.findByPk(id);
+
+        if (!book) {
+            
+            /*
+             * Without 'return' this function will not terminate,
+             * it would try to send another header response below and 
+             * cause an error:
+             *
+             * "Error [ERR_HTTP_HEADERS_SENT]: 
+             * Cannot set headers after they are sent to the client"
+            */
+            return res.status(404)
+               .json("The book doesn't exist.");
+        }
+
+        res.status(200)
+           .json(book);
+
+    } catch (error) {
+
+        res.status(500)
+           .json({ error: error.message });
+    }
+})
+
+// POST-ENDPOINT: 'add book'
 app.post('/api/books', async (req, res) => {
 
     try {
@@ -37,11 +89,40 @@ app.post('/api/books', async (req, res) => {
 
     } catch (error) {
         
-        // Handle error in a general way for now
         res.status(500)
-           .json({
-                error: error.message
-            });
+           .json({ error: error.message });
+    }
+});
+
+// DELETE-ENDPOINT: 'delete a record'
+app.delete('/api/books/:id', async (req, res) => {
+
+    try {
+
+        const bookId = req.params.id;
+        const numOfDestroyedRecords = await Book.destroy({
+            where: { id: bookId }
+        });
+
+        /*
+         * .send() is an another method used to
+         * send a response to the client.
+         * As opposed to .json(), it doesn't send a
+         * body JSON, which is not required here.
+        */
+
+        if (numOfDestroyedRecords === 0) {
+            return res.status(404)
+                      .send();
+        } 
+        
+        res.status(204)
+           .send();
+
+    } catch (error) {
+
+        res.status(500)
+           .json({ error: error.message });
     }
 });
 
