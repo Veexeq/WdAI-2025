baa1e8de4a9a48b874e02785120a2ac663aa3d81
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-06 22:07:16 +0100

    Added POST request at /app/register endpoint. This solution doesn't have JWT implemented yet.

--- Lab4/MikroserwisyProjekt/users-service/app.js
+++ Lab4/MikroserwisyProjekt/users-service/app.js
@@ -8,6 +8,65 @@ const User = require('./models/User');
 // We will need to read requests' bodies
 app.use(express.json());
 
+app.post('/api/register', async (req, res) => {
+
+    try {
+
+        // Deconstruct the request's body JSON
+        const { email, password } = req.body;
+
+        /**
+         * For future reference, as I had some issues with wrapping my
+         * head around this: newAccount is an object holding a newly
+         * created record in the User table.
+         * 
+         * This is crutial when sending the response (read the comment
+         * regarding res.(...).json(...) below!)
+         */
+        const newAccount = await User.create({
+            email: email,
+            password: password
+        });
+
+        /**
+         * In response's body we should return the least amount of info
+         * necessary for the reciever. For example, front-end doesn't need
+         * password-hash-related info.
+         * 
+         * In regard to the comment above: newAccount is a record in the
+         * DB (as a result it has an id, even though 'req.body' only
+         * had 'email' and 'password' attributes).
+         */
+        res.status(201).json({
+            id: newAccount.id,
+            email: newAccount.email
+        });
+
+    } catch (error) {
+
+        if (error.name === 'SequelizeUniqueConstraintError') {
+            return res.status(400).json({
+                error: "An account with this e-mail address already exists"
+            });
+        }
+
+        if (error.name === 'SequelizeValidationError') {
+            return res.status(400).json({
+                error: "Invalid data",
+
+                // Return a list of error messages
+                details: error.errors.map(e => e.message)
+            });
+        }
+
+        // Handle all other exceptions
+        console.error(error);
+        return res.status(500).json({ 
+            error: "Unhandled exception" 
+        });
+    }
+});
+
 sequelize.sync().then(() => {
     console.log('Sequalize has been initialized.');
 
