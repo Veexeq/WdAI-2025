58f81e5b9496f3ea04f19d0c4b70b660d3b30ebb
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-06 23:49:23 +0100

    Added POST login endpoint at /api/login. It returns a JWT correctly.

--- Lab4/MikroserwisyProjekt/users-service/app.js
+++ Lab4/MikroserwisyProjekt/users-service/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3003;
 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const sequelize = require('./config/database');
 const User = require('./models/User');
 
@@ -63,6 +65,82 @@ app.post('/api/register', async (req, res) => {
         console.error(error);
         return res.status(500).json({ 
             error: "Unhandled exception" 
+        });
+    }
+});
+
+app.post('/api/login', async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body;
+        const user = await User.findOne({
+            where: { email: email }
+        });
+
+        if (!user) {
+            return res.status(401).json({
+                error: "Invalid credentials"
+            });
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) {
+            return res.status(401).json({
+                error: "Invalid credentials"
+            });
+        }
+
+        /**
+         * If the credentials match, we generate a JWT to authorize
+         * further operations (remember that HTTP is stateless).
+         * 
+         * JSON Web Token (JWT) matches the following pattern: A.B.C, where:
+         * 1. 'A' is the header section, it consists of technical info about the
+         * algorithm used in signing the token etc.
+         * 2. 'B' is the payload, i.e. a JSON object that stores information 
+         * necessaary to identify the user associated with this specific JWT
+         * alongside other things (see note below).
+         * 3. 'C' is the signature, which plays the role of a private key.
+         * 
+         * JWT is used in authentication in following manner:
+         * 1. Parse the technical info in headers
+         * 2. Identify the target via the payload
+         * 3. See whether the signature of this token matches the 
+         * signature of the target stored on the server's DB
+         * 
+         * IMPORTANT: header and payload is encoded, not hashed. Everyone can
+         * decode it and retrieve the data inside. Therefore, only public information
+         * (like ids) necessary for user recognition should be stored in the paylod!
+         * 
+         * Note: payload does not only consist of the info necessary for 
+         * recognizing the user. Its attributes are called 'claims' and the standard
+         * payload looks something like this:
+         * {
+         *  sub: subjectID,
+         *  iat: issuedAt, (when was the token created)
+         *  exp: expirationDate
+         * }
+         * 
+         * Method info:
+         * jwt.sign(...) is most commonly used with three args: 
+         * payload, secretKey, options
+         */
+        const token = jwt.sign(
+            { sub: user.id },
+            'mock_secret_key',
+            { expiresIn: '1h' }
+        );
+
+        return res.status(200).json({
+            message: 'Logged in succesfully',
+            token: token
+        });
+
+    } catch (error) {
+
+        return res.status(500).json({
+            error: "Unhandled exception"
         });
     }
 });
