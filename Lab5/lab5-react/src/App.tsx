83a508b0fc6c8fb3e8bf5bd5ab957ed895dd3823
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-14 13:03:17 +0100

    Cleaned up App.tsx

--- Lab5/lab5-react/src/App.tsx
+++ Lab5/lab5-react/src/App.tsx
@@ -1,25 +1,7 @@
 import './App.css'
-import Welcome from './components/welcome';
-import type { WelcomeObject } from './types/welcomeComponent';
 
 function App() {
-
-  const welcomeObject: WelcomeObject = {
-    name: "Wiktor",
-    age: 20
-  }
-
-  const welcomeObjectFemale: WelcomeObject = {
-    name: "Ania",
-    age: 21
-  };
-
-  return (
-    <>
-      <Welcome name={welcomeObject.name} age={welcomeObject.age} />
-      <Welcome name={welcomeObjectFemale.name} age={welcomeObjectFemale.age} />
-    </>
-  )
+  return <></>
 }
 
 export default App
