13e21474bb616a10fafa386b73bbb29843adc1f2
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-14 14:59:33 +0100

    Explored basics of useState, completed Exercise 2.1

--- Lab5/lab5-react/src/components/liczniki/Counter.tsx
+++ Lab5/lab5-react/src/components/liczniki/Counter.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import styles from './counter.module.css'
 
 function Counter() {
+  const [counter, setCounter] = useState(0); 
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <div className={styles.counterWrapper}>
-      <p className={styles.counterText} id="counterText">0</p>
-      <button className={styles.counterButton}>Click me</button>
+      <p className={styles.counterText} id="counterText">{counter}</p>
+      <button className={styles.counterButton} onClick={incrementCounter}>Click me</button>
     </div>
   );
 
