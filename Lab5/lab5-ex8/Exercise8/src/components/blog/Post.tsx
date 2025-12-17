449137acc7643e4b5b52e07558cb54bc31700629
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-17 23:34:47 +0100

    Finished post showcase page

--- Lab5/lab5-ex8/Exercise8/src/components/blog/Post.tsx
+++ Lab5/lab5-ex8/Exercise8/src/components/blog/Post.tsx
@@ -1,12 +1,32 @@
 import { useParams } from "react-router-dom";
+import type { post } from "../../types/post";
+import styles from './post.module.css';
 
 function Post() {
 
   const { id } = useParams();
+  let post: post | null = null;
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const key: string | null = localStorage.key(i);
+    if (key && key === id) {
+      post = JSON.parse(localStorage.getItem(key)!);
+      break;
+    }
+  }
+
+  if (!post) {
+    return (<h1>404 NOT FOUND</h1>);
+  }
 
   return (
     <>
-      <h1>{id}</h1>  
+      <div className={styles.wrapper}>
+        <div className={styles.card}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      </div>
     </>
   );
 }
